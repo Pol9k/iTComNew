d2d881c9a9148769f8921efa3a321fe566826c44
Andrej <bap2585@gmail.com>
2021-10-21 16:57:32 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,4 +4,11 @@ const navbar = document.querySelector('.header__row-navigation');
 
 burger.addEventListener("click", () => {
   navbar.classList.toggle('changeMenu');
-})
+});
+
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  // When the scroll is higher than 460 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if(this.scrollY >= 460) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
